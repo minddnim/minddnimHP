ff76965b391c2ad5821978059820275dca5b7026
mind mind <minddnim@gmail.com>
2014-03-17 23:06:39 +0900

    皿難易度表のデータをコミット

---
+++ update.bat
@@ -0,0 +1,17 @@
+cd bms_sara_SSPtRanking_total
+start FromHPtoSSPtData300.exe
+cd ..\
+cd bms_sara_SSPtRanking_easy
+start FromHPtoSSPtData_easy.exe
+cd ..\
+cd bms_sara_SSPtRanking_normal
+start FromHPtoSSPtData_normal.exe
+cd ..\
+cd bms_sara_SSPtRanking_hard
+start FromHPtoSSPtData_hard.exe
+cd ..\
+cd bms_sara_ClearRanking
+start FromHPtoAllPlayerData300.exe
+cd ..\
+cd bms_sara_ClearData
+start FromHPtoClearCount.exe
